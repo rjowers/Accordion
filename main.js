f887167f56b04d17ced0003809434c274e6d8a38
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-07 21:45:13 -0500

    finished project

--- main.js
+++ main.js
@@ -8,12 +8,32 @@
      $(this).removeClass('goaway');
  });
 */
-$(".words1").addClass("goaway");
 
-function click1 () {
-    $(".words1").removeClass('goaway');
-      $(".words2").addClass('goaway');
-      $(".words3").addClass('goaway');
+
+$("section").on("mouseover", expandBox);
+
+function expandBox(event){
+  var target = event.currentTarget;
+  target = $(target);
+  $(".section1").removeClass("expanded").addClass("shrunk");
+  $(".section2").removeClass("expanded").addClass("shrunk");
+  $(".section3").removeClass("expanded").addClass("shrunk");
+  target.removeClass("shrunk noClass").addClass( "expanded" );
+}
+
+$(".words1").addClass("close");
+
+
+var goaway
+function click1 (event) {
+    $(".words1").removeClass('close').addClass('open');
+    $(".words2").addClass('close').removeClass('open');
+    $(".words3").addClass('close').removeClass('open');
+
+
+      //$(".words1").addClass('open');
+      //$(".words2").addClass('close');
+      //$(".words3").addClass('close');
 }
 
 $(".section1").on('click',click1);
@@ -21,12 +41,14 @@ $(".section1").on('click',click1);
 
 
 
-$(".words2").addClass("goaway");
+$(".words2").addClass("close");
 
+var goaway2
 function click2 () {
-    $(".words2").removeClass('goaway');
-    $(".words1").addClass('goaway');
-    $(".words3").addClass('goaway');
+  $(".words2").removeClass('close').addClass('open');
+  $(".words1").addClass('close').removeClass('open');
+  $(".words3").addClass('close').removeClass('open');
+
 }
 
 $(".section2").on('click',click2);
@@ -34,12 +56,14 @@ $(".section2").on('click',click2);
 
 
 
-$(".words3").addClass("goaway");
+$(".words3").addClass("close");
 
+var goaway3
 function click3 () {
-    $(".words3").removeClass('goaway');
-    $(".words2").addClass('goaway');
-    $(".words1").addClass('goaway');
+  $(".words3").removeClass('close').addClass('open');
+  $(".words2").addClass('close').removeClass('open');
+  $(".words1").addClass('close').removeClass('open');
+
 }
 
 $(".section3").on('click',click3);
