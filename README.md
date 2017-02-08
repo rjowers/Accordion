0109b5df31d661abc347ba4788b7ac00a643f4b9
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-08 09:16:27 -0500

    added README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+#Accordion
+In this excersise we were given the task of created a animated sliding menu. This project was designed to make us
+understand how to use jQuery to manipulate the DOM and
+how CSS transitions work and why they are beneficial.
--- main.js
+++ main.js
@@ -10,16 +10,7 @@
 */
 
 
-$("section").on("mouseover", expandBox);
 
-function expandBox(event){
-  var target = event.currentTarget;
-  target = $(target);
-  $(".section1").removeClass("expanded").addClass("shrunk");
-  $(".section2").removeClass("expanded").addClass("shrunk");
-  $(".section3").removeClass("expanded").addClass("shrunk");
-  target.removeClass("shrunk noClass").addClass( "expanded" );
-}
 
 $(".words1").addClass("close");
 
